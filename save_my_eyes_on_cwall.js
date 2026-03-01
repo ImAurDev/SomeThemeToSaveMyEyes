2b64fa0021bec3969e3471fdd5532825ae54057a
ImAur <imaur@foxmail.com>
2026-03-01 15:48:55 +0800

    feat. 拯救我看Cwall时的眼睛

---
+++ save_my_eyes_on_cwall.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Cwall主题
+// @namespace    https://github.com/ImAurDev/SaveMyEves
+// @version      2026-03-01
+// @description  给Cwall做的主题
+// @author       ImAur
+// @match        https://c4.cwall.xyz/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    "use strict";
+
+    const styleElement = document.createElement("style");
+    styleElement.innerHTML = `
+    `;
+})();
